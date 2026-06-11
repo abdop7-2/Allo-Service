2f1c39669d9a2fd5a9290e0f9c5ab1e8e47bf635
adex576 <allem9667@gmail.com>
2026-06-11 03:14:38 +0100

    switch baseURL back to ngrok

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.11.116:8000',
-  headers: { Accept: 'application/json' },
+  baseURL: 'https://morale-bacterium-avid.ngrok-free.dev',
+  headers: { Accept: 'application/json', 'ngrok-skip-browser-warning': 'true' },
 });
 
 api.interceptors.request.use((config) => {
